f419f1d3bd484a0f9a84ff4b520b3999bbd24cdf
Fajri <owltility@gmail.com>
2025-05-28 06:23:49 +0700

    mebuat folder wpu-pizza hut, membuat folder data yang berisi pizza.json

---
+++ json/js/script.js
@@ -0,0 +1,22 @@
+// let mahasiswa = {
+//     nama: "Muhammad Nurul Fajri",
+//     nim : "2217020054",
+//     email : "muhammadnurulfajri1602@gmail.com"
+// }
+
+// console.log(JSON.stringify(mahasiswa));
+
+
+// let xhr = new XMLHttpRequest(); 
+// xhr.onreadystatechange = function () { 
+//     if (xhr.readyState == 4 && xhr.status == 200) { 
+//         let mahasiswa = JSON.parse(this.responseText); 
+//         console.log(mahasiswa); 
+//     } 
+// } 
+// xhr.open('GET', 'coba.json', true); 
+// xhr.send();\
+
+$.getJSON('coba.json', function (data){
+    console.log(data);
+});
